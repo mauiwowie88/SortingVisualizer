3a363d3bd2b6671d270a36e777edd21b90e77273
mauiwowie88 <maugarza96@gmail.com>
2023-05-30 16:44:27 -0700

    Sloth maneuvers.

--- sortingvisualizer/src/App.js
+++ sortingvisualizer/src/App.js
@@ -6,26 +6,16 @@ import logo from './images/logo.png';
 
 function App() {
     const [array, setArray] = useState([]);
-    const [size, setSize] = useState(22);
-    const [speed, setSpeed] = useState(100);
-    const [waiting, setWaiting] = useState();
+    const [size, setSize] = useState(100);
+    const [speed, setSpeed] = useState(10);
     
     function generateArray() {
       const newArray = Array.from({ length: size }, () => Math.random());
       setArray(newArray);
     }
 
-    function randomizer() {
-      const randomIndex = Math.floor(Math.random() * size);
-      const randomHeight = Math.random();
-      // newArray[randomIndex] = randomHeight;
-      // showBars();
-      console.log('hello')
-    }
-
     function start() {
       generateArray();
-      showBars();
     }
     
     function sort() {
@@ -35,31 +25,23 @@ function App() {
     }
   
     function animate(moves) {
-      if (moves.length === 0) {
-        showBars();
-        return;
-      }
+      if (moves.length === 0) return;
   
-      const move = moves.shift();
+      const copy = [...array];
+      const move = moves[0];
       const [i, j] = move.indices;
   
-      if (move.type === "swap") {
-        [array[i], array[j]] = [array[j], array[i]];
-      }
+      if (move.type === "swap") [array[i], array[j]] = [array[j], array[i]];
   
-      // playNote(200 + array[i] * 400);
-      // playNote(200 + array[j] * 400);
-  
-      showBars(move);
       setTimeout(function () {
-        animate(moves);
+        setArray(copy);
+        animate(moves.slice(1));
       }, speed);
     }
 
     const handleSizeChange = (event) => {
       const newSize = parseInt(event.target.value);
       setSize(newSize);
-      showBars();
     };
 
     const handleSpeedChange = (event) => {
@@ -83,65 +65,52 @@ function App() {
       return moves;
     }
 
-    function showBars(move) {
-      const container = document.getElementById("container");
-      container.innerHTML = "";
-      const maxWidth = 100 / size;
-
-      for (let i = 0; i < size; i++) {
-        const bar = document.createElement("div");
-        const barHeight = array[i] * 100;
-        const barWidth = maxWidth + "%";
-
-        bar.classList.add("bar");
-        bar.style.height = `${barHeight}%`;
-        bar.style.width = barWidth;
-        bar.innerHTML = barHeight.toFixed(0);
-    
-        if (move && move.indices.includes(i)) {
-          bar.style.backgroundColor =
-            move.type === "swap" ? "green" : "red";
-        }
-        container.appendChild(bar);
-      }
-    }
-
     useEffect(() => {
       generateArray();
-      showBars();
-      randomizer();
     }, [size]);
 
     return (
-      <div id="home">
-          <header>
-            <img src={logo} alt="logo" style={{ width: '240px' }}/>
-            <div class="menu-bar">
-              <i class="fa-solid fa-bars menu-button"></i>
-              <ul class="dropdown-menu">
-                <button class="btn">Bubble</button>
-                <button class="btn">Selection</button>
-                <button class="btn">Insertion</button>
-                <button class="btn">Merge</button>
-                <button class="btn">Quick</button>
-              </ul>
-            </div>
-          </header>
+      <div>
+        <header>
+          <img src={logo} alt="logo" style={{ width: '240px' }}/>
+            
+          <div className="dropdown-menu">
+            <button className="btn">Bubble</button>
+            <button className="btn">Selection</button>
+            <button className="btn">Insertion</button>
+            <button className="btn">Merge</button>
+            <button className="btn">Quick</button>
+          </div>
+            <i className="fa-solid fa-bars menu-button"></i>
+        </header>
 
         <div id="main">
-          <div id="container"></div>
+          <div id="visualizer">
+            {array.map((barHeight, index) => (
+              <div
+                key={index}
+                className="bar"
+                style={{
+                  height: `${barHeight * 100}%`,
+                  "--bar-height": barHeight * 100
+                }}
+              >
+                {index.toFixed(0)}
+              </div>
+            ))}
+          </div>
           <div id="settings">
-            <button class="btn" onClick={start}>Start</button>
-            <button class="btn" onClick={sort}>Sort</button>
-            <button class="sliderBtn"><a>Size: </a><span>{size}</span></button>
+            <button className="btn" onClick={start}>Start</button>
+            <button className="btn" onClick={sort}>Sort</button>
+            <button className="sliderBtn">Size: <span>{size}</span></button>
             <input
               type="range"
               id="size_slider"
-              min="4" max="100"
+              min="5" max="250"
               value={size}
               onChange={handleSizeChange}
             />
-            <button class="sliderBtn"><a>Speed: </a><span>{speed}</span></button>
+            <button className="sliderBtn">Speed: <span>{speed}</span></button>
             <input 
               type="range" 
               id="speed_slider" 
