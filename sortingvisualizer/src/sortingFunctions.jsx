a3c29b9d9bad3624af61ad3e11e056e8fd4479ca
mauiwowie88 <maugarza96@gmail.com>
2023-05-31 01:43:11 -0700

    Update code formatting

--- sortingvisualizer/src/sortingFunctions.jsx
+++ sortingvisualizer/src/sortingFunctions.jsx
@@ -1,17 +1,17 @@
 /*** Sorting Functions ***/
 
 export const bubbleSort = (array) => {
-	const moves = [];
-	let swapped;
-	do {
-		swapped = false;
-		for (let i = 1; i < array.length; i++) {
-			if (array[i - 1] > array[i]) {
-				swapped = true;
-				moves.push({ indices: [i - 1, i], type: "swap" });
-				[array[i - 1], array[i]] = [array[i], array[i - 1]];
-			}
-		}
-	} while (swapped);
-	return moves;
+  const moves = [];
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 1; i < array.length; i++) {
+      if (array[i - 1] > array[i]) {
+        swapped = true;
+        moves.push({ indices: [i - 1, i], type: "swap" });
+        [array[i - 1], array[i]] = [array[i], array[i - 1]];
+      }
+    }
+  } while (swapped);
+  return moves;
 };
