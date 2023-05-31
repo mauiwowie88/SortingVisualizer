aee809d963e44f7951e0c6f2ff35ef30134f8825
mauiwowie88 <maugarza96@gmail.com>
2023-05-31 01:49:27 -0700

    Added speed slider.

--- sortingvisualizer/src/Components/Settings.jsx
+++ sortingvisualizer/src/Components/Settings.jsx
@@ -1,48 +1,46 @@
-import React from "react";
-import "../App.css";
 import Visualizer from "./Visualizer";
 
 function Settings({
-  array,
-  moves,
-  start,
-  sort,
-  size,
-  handleSizeChange,
-  speed,
-  handleSpeedChange,
+	array,
+	moves,
+	start,
+	sort,
+	size,
+	handleSizeChange,
+	speed,
+	handleSpeedChange,
 }) {
-  return (
-    <div id="main">
-      <Visualizer array={array} moves={moves} />
-      <div id="settings">
-        <button onClick={start}>Start</button>
-        <button onClick={sort}>Sort</button>
-        <button className="sliderBtn">
-          Size: <span>{size}</span>
-        </button>
-        <input
-          type="range"
-          id="size_slider"
-          min="11"
-          max="300"
-          value={size}
-          onChange={handleSizeChange}
-        />
-        <button className="sliderBtn">
-          Speed: <span>{speed}</span>
-        </button>
-        <input
-          type="range"
-          id="speed_slider"
-          min="-100000"
-          max="111"
-          value={speed}
-          onChange={handleSpeedChange}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div id="main">
+			<Visualizer array={array} moves={moves} />
+			<div id="settings">
+				<button onClick={start}>Start</button>
+				<button onClick={sort}>Sort</button>
+				<button className="sliderBtn">
+					Size: <span>{size}</span>
+				</button>
+				<input
+					type="range"
+					id="size_slider"
+					min="11"
+					max="300"
+					value={size}
+					onChange={handleSizeChange}
+				/>
+				<button className="sliderBtn">
+					Speed: <span>{speed}</span>
+				</button>
+				<input
+					type="range"
+					id="speed_slider"
+					min="-100000"
+					max="111"
+					value={speed}
+					onChange={handleSpeedChange}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default Settings;
--- sortingvisualizer/src/Components/Visualizer.jsx
+++ sortingvisualizer/src/Components/Visualizer.jsx
@@ -1,28 +1,25 @@
-import React from "react";
-
 function Visualizer({ array, moves }) {
-  return (
-    <div id="visualizer">
-      {array.map((barHeight, index) => (
-        <div
-          key={index}
-          className={`bar ${
-            moves.length > 0 &&
-            moves[0].indices.includes(index) &&
-            moves[0].type === "swap"
-              ? "green"
-              : "red"
-          }`}
-          style={{
-            height: `${barHeight * 100}%`,
-            "--bar-height": barHeight * 100,
-          }}
-        >
-          {(barHeight * 100).toFixed(0)}
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div id="visualizer">
+			{array.map((barHeight, index) => (
+				<div
+					key={index}
+					className={`bar ${
+						moves.length > 0 &&
+						moves[0].indices.includes(index) &&
+						moves[0].type === "swap"
+							? "green"
+							: "red"
+					}`}
+					style={{
+						height: `${barHeight * 100}%`,
+						"--bar-height": barHeight * 100,
+					}}>
+					{(barHeight * 100).toFixed(0)}
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default Visualizer;
