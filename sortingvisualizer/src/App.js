b6dab9929cf4ff312a5e0161deffd7b27266d627
mauiwowie88 <maugarza96@gmail.com>
2023-06-02 14:57:46 -0700

    added more sorting functions

--- sortingvisualizer/src/App.js
+++ sortingvisualizer/src/App.js
@@ -2,14 +2,20 @@ import "./App.css";
 import logo from "./images/logo.png";
 import React, { useState, useEffect } from "react";
 import Settings from "./Components/Settings";
-import { bubbleSort } from "./sortingFunctions";
+import {
+	bubbleSort,
+	insertionSort,
+	mergeSort,
+	quickSort,
+	selectionSort,
+} from "./sortingFunctions";
 
 function App() {
 	const [array, setArray] = useState([]);
 	const [size, setSize] = useState(11);
 	const [speed, setSpeed] = useState(111);
-	const [moves, setMoves] = useState([]);
-	const [colorIndices, setColorIndices] = useState([]);
+	// const [moves, setMoves] = useState([]);
+	// const [swap, setSwap] = useState("");
 
 	function generateArray() {
 		const newArray = Array.from({ length: size }, () => Math.random());
@@ -43,55 +49,23 @@ function App() {
 		const move = moves[0];
 		const [i, j] = move.indices;
 		const bars = document.getElementsByClassName("bar");
-
-		console.log(move.indices);
-
 		if (move.type === "swap") {
 			[array[i], array[j]] = [array[j], array[i]];
+			bars[i].style.backgroundColor = "red";
+		} else {
 			bars[i].style.backgroundColor = "green";
-			bars[j].style.backgroundColor = "green";
-		} else if (move.type === "compare") {
-			bars[i].style.backgroundColor = "blue";
-			bars[j].style.backgroundColor = "blue";
 		}
 
-		setArray(copy);
-
 		setTimeout(function () {
-			setArray(copy);
 			bars[i].style.backgroundColor = "cyan";
-			bars[j].style.backgroundColor = "cyan";
-
 			animate(moves.slice(1));
 		}, speed);
-	}
-
-	function bubbleSort(array) {
-		const moves = [];
-		let swapped;
-		do {
-			swapped = false;
-			for (let i = 1; i < array.length; i++) {
-				if (array[i - 1] > array[i]) {
-					swapped = true;
-					moves.push({ indices: [i - 1, i], type: "swap" });
-					[array[i - 1], array[i]] = [array[i], array[i - 1]];
-				} else {
-					moves.push({ indices: [i - 1, i], type: "compare" });
-				}
-			}
-		} while (swapped);
-		return moves;
+		setArray(copy);
 	}
 
 	useEffect(() => {
 		generateArray();
 	}, [size]);
-
-	useEffect(() => {
-		const colorIndices = moves.map((move) => move.indices).flat();
-		setColorIndices(colorIndices);
-	}, [moves]);
 
 	return (
 		<div id="app">
@@ -114,7 +88,7 @@ function App() {
 					{array.map((value, index) => (
 						<div
 							key={index}
-							className={`bar ${colorIndices.includes(index) ? "swap" : "compare"}`}
+							className="bar"
 							style={{
 								height: `${value * 100}%`,
 							}}>
--- sortingvisualizer/src/sortingFunctions.jsx
+++ sortingvisualizer/src/sortingFunctions.jsx
@@ -10,19 +10,97 @@ export const bubbleSort = (array) => {
 				swapped = true;
 				moves.push({ indices: [i - 1, i], type: "swap" });
 				[array[i - 1], array[i]] = [array[i], array[i - 1]];
-				console.log("yes", moves[i]);
 			} else {
-				console.log("noo", moves[moves.length - 1]?.type);
+				moves.push({ indices: [i - 1, i], type: "compare" });
 			}
 		}
 	} while (swapped);
 	return moves;
 };
 
-export const insertionSort = (array) => {};
+export const insertionSort = (array) => {
+	const moves = [];
+	for (let i = 1; i < array.length; i++) {
+		let j = i;
+		while (j > 0 && array[j - 1] > array[j]) {
+			moves.push({ indices: [j - 1, j], type: "swap" });
+			[array[j - 1], array[j]] = [array[j], array[j - 1]];
+			j--;
+		}
+		moves.push({ indices: [j, j + 1], type: "compare" });
+	}
+	return moves;
+};
 
-export const mergeSort = (array) => {};
+export const mergeSort = (array) => {
+	const moves = [];
 
-export const quickSort = (array) => {};
+	const merge = (left, right) => {
+		const sorted = [];
+		while (left.length && right.length) {
+			const smaller = left[0] < right[0] ? left : right;
+			sorted.push(smaller.shift());
+			moves.push({ indices: [left.length, right.length], type: "compare" });
+		}
+		return sorted.concat(left.length ? left : right);
+	};
 
-export const selectionSort = (array) => {};
+	const sort = (array) => {
+		if (array.length <= 1) return array;
+		const mid = Math.floor(array.length / 2);
+		return merge(sort(array.slice(0, mid)), sort(array.slice(mid)));
+	};
+
+	sort(array);
+	return moves;
+};
+
+export const quickSort = (array) => {
+	const moves = [];
+
+	const partition = (array, low, high) => {
+		const pivot = array[high];
+		let i = low;
+		for (let j = low; j < high; j++) {
+			if (array[j] < pivot) {
+				[array[i], array[j]] = [array[j], array[i]];
+				moves.push({ indices: [i, j], type: "swap" });
+				i++;
+			} else {
+				moves.push({ indices: [i, j], type: "compare" });
+			}
+		}
+		[array[i], array[high]] = [array[high], array[i]];
+		moves.push({ indices: [i, high], type: "swap" });
+		return i;
+	};
+
+	const sort = (array, low, high) => {
+		if (low < high) {
+			const pivotIndex = partition(array, low, high);
+			sort(array, low, pivotIndex - 1);
+			sort(array, pivotIndex + 1, high);
+		}
+	};
+
+	sort(array, 0, array.length - 1);
+	return moves;
+};
+
+export const selectionSort = (array) => {
+	const n = array.length;
+
+	for (let i = 0; i < n - 1; i++) {
+		let minIndex = i;
+		for (let j = i + 1; j < n; j++) {
+			if (array[j] < array[minIndex]) {
+				minIndex = j;
+			}
+		}
+		if (minIndex !== i) {
+			[array[i], array[minIndex]] = [array[minIndex], array[i]];
+		}
+	}
+
+	return array;
+};
